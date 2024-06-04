f40800b59397d3f092f5b1593456139ffe8158f6
Honzikoi <honza.doan@gmail.com>
2024-06-05 00:45:57 +0200

    JWT login tests

--- cmd/routes.go
+++ cmd/routes.go
@@ -11,10 +11,13 @@ import (
 func setupRoutes(app *fiber.App) {
 	// Public routes
 	app.Get("/", handlers.Home)
-	app.Post("/signup", handlers.SignUp)
 	app.Post("/login", handlers.Login)
-	app.Get("/auth/google", handlers.GoogleLogin)
-	app.Get("/auth/google/callback", handlers.GoogleCallback)
+
+    app.Use("/profile", jwt.New(jwt.Config{
+        SigningKey: []byte(os.Getenv("SECRET_KEY")),
+        ContextKey: "user", // This is the key to access the user in the handlers
+    }))
+    app.Get("/profile", handlers.Profile)
 
 	// JWT Protected routes
 	protected := app.Group("/", jwt.New(jwt.Config{
