671a9a75bead4aff80605ed3bd43ac402861b5b2
Honzikoi <honza.doan@gmail.com>
2024-06-17 01:02:53 +0200

    small hotfix

--- cmd/routes.go
+++ cmd/routes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/Honzikoi/gym-challenge/handlers"
+	"github.com/Honzikoi/gym-challenge/middlewares"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -11,6 +12,10 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/", handlers.Home)
 	app.Post("/signup", handlers.SignUp)
 	app.Post("/login", handlers.Login)
+	// Add your middleware to protect the profile route
+	app.Use("/profile", middlewares.Protected())
+
+	// Register the profile route
 	app.Get("/profile", handlers.Profile)
 	// app.Get("/auth/google", handlers.GoogleLogin)
 	// app.Get("/auth/google/callback", handlers.GoogleCallback)
