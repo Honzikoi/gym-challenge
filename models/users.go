bd8bc594df70b72c1409520f73cdc8741895a4aa
Honzikoi <honza.doan@gmail.com>
2024-07-16 03:36:28 +0200

    Hotfix for deployment#3

--- models/users.go
+++ models/users.go
@@ -9,5 +9,13 @@ type Users struct {
 	Email    string `json:"email" gorm:"text;not null"`
 	Username string `json:"username" gorm:"text;not null"`
 	Password string `json:"password" gorm:"text;not null"`
-	Roles  []Role   `json:"role_id" gorm:"foreignKey:UserID"`
+	Roles  []Role   `json:"role_id" gorm:"foreignKey:UserID;references:ID"`
+}
+
+type Role struct {
+	gorm.Model
+	Name        string `json:"name" gorm:"text;not null"`
+	Description string `json:"description" gorm:"text;not null"`
+	Permissions string `json:"permissions" gorm:"text;not null"`
+	Users []Users `gorm:"many2many:user_roles;"`
 }
