792fd5ed499fffeec258a24c53fd45497d5eac17
Honzikoi <honza.doan@gmail.com>
2024-07-17 14:52:52 +0200

    Overwriting fixtures permission on prod

--- database/database.go
+++ database/database.go
@@ -73,7 +73,7 @@ func ConnectDb() {
 		Db: db,
 	}
 
-	// Get the standard database connection from GORM
+	// Get the standard database connection from GORM for the fixtures
 	sqlDB, err := db.DB()
 	if err != nil {
 		log.Fatalf("Failed to get the standard database connection: %v", err)
