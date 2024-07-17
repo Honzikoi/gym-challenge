c3787841b7cbfea9a208f55c1149b0949f418ee4
Honzikoi <honza.doan@gmail.com>
2024-07-17 05:35:52 +0200

    Mini-hotfix for deployment

--- models/sessions.go
+++ models/sessions.go
@@ -9,7 +9,7 @@ type Sessions struct {
 	Name        string    `json:"name" gorm:"text;not null;default:null"`
 	Description string    `json:"user_id" gorm:"text;not null"`
 	Group_id    int       `json:"group_id" gorm:"int;not null"`
-	Workouts    []Workout `json:"workouts"`
+	Workouts    []Workout `json:"workouts" gorm:"foreignKey:SessionID"`
 }
 
 type UserGroup struct {
