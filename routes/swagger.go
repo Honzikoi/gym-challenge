6f15d17f33d89906223af76c5bfd914758814d4e
Honzikoi <honza.doan@gmail.com>
2024-07-18 03:32:41 +0200

    Swagger Hotfix for Heroku#2

--- routes/swagger.go
+++ routes/swagger.go
@@ -6,20 +6,20 @@ import (
 )
 
 func SwaggerRoutes(app *fiber.App) {
-	// app.Get("/swagger/*", swagger.HandlerDefault)
-	app.Get("/swagger/*", swagger.New(swagger.Config{ // custom
-		URL:         "https://go-gym-challenge-2c47afc55a78.herokuapp.com/swagger/doc.json",
-		DeepLinking: false,
-		// Expand ("list") or Collapse ("none") tag groups by default
-		DocExpansion: "none",
-		// Prefill OAuth ClientId on Authorize popup
-		OAuth: &swagger.OAuthConfig{
-			AppName:  "OAuth Provider",
-			ClientId: "21bb4edc-05a7-4afc-86f1-2e151e4ba6e2",
-		},
-		// Ability to change OAuth2 redirect uri location
-		OAuth2RedirectUrl: "http://localhost:3000/swagger/oauth2-redirect.html",
-	}))
+	app.Get("/swagger/*", swagger.HandlerDefault)
+	// app.Get("/swagger/*", swagger.New(swagger.Config{ // custom
+	// 	URL:         "https://go-gym-challenge-2c47afc55a78.herokuapp.com/swagger/doc.json",
+	// 	DeepLinking: false,
+	// 	// Expand ("list") or Collapse ("none") tag groups by default
+	// 	DocExpansion: "none",
+	// 	// Prefill OAuth ClientId on Authorize popup
+	// 	OAuth: &swagger.OAuthConfig{
+	// 		AppName:  "OAuth Provider",
+	// 		ClientId: "21bb4edc-05a7-4afc-86f1-2e151e4ba6e2",
+	// 	},
+	// 	// Ability to change OAuth2 redirect uri location
+	// 	OAuth2RedirectUrl: "http://localhost:3000/swagger/oauth2-redirect.html",
+	// }))
 
 	// Serve Swagger JSON
 	app.Static("/swagger/doc.json", "./docs/swagger.json")
