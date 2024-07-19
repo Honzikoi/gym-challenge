d92cbd6c73139c2740853a8ad46b3ef0b40dacb0
Honzikoi <honza.doan@gmail.com>
2024-07-19 13:30:04 +0200

    Unlinking relations for unnecessary issues

--- models/group.go
+++ models/group.go
@@ -8,6 +8,5 @@ type Group struct {
 	gorm.Model
 	SessionID uint       `json:"session_id"`
 	Name      string     `json:"name" gorm:"type:varchar(255)"`
-	Users     []Users    `json:"users" gorm:"many2many:user_groups;"`
 	Sessions  []Sessions `json:"sessions"`
 }
