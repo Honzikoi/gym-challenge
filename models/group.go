f997cf6b58e943cfc36bb2a5b116b78f6acac577
Honzikoi <honza.doan@gmail.com>
2024-07-19 13:08:22 +0200

    Hotfixing sessions for deployment#3

--- models/group.go
+++ models/group.go
@@ -1,4 +1,3 @@
-// models/groups.go
 package models
 
 import (
@@ -7,7 +6,8 @@ import (
 
 type Group struct {
 	gorm.Model
-	GroupID uint    `json:"group_id"`
-	Name    string  `json:"name" gorm:"type:varchar(255)"`
-	Users   []Users `json:"users" gorm:"many2many:user_groups;"`
+	SessionID uint       `json:"session_id"`
+	Name      string     `json:"name" gorm:"type:varchar(255)"`
+	Users     []Users    `json:"users" gorm:"many2many:user_groups;"`
+	Sessions  []Sessions `json:"sessions"`
 }
