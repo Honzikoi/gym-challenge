0a398c98c33b4536c13d2b8185099eb770a0f861
Honzikoi <honza.doan@gmail.com>
2024-07-19 14:53:50 +0200

    Hotfix for Sessions#3

--- models/group.go
+++ models/group.go
@@ -6,9 +6,7 @@ import (
 
 type Group struct {
 	gorm.Model
-	SessionID uint    `json:"session_id"`
-	Name      string  `json:"name" gorm:"type:varchar(255)"`
-	UserID    uint    `json:"user_id"`
-	User      Users   `json:"user_creator" gorm:"foreignKey:UserID"`
-	Users     []Users `json:"users" gorm:"many2many:user_groups;"`
+	SessionID uint   `json:"session_id"`
+	Name      string `json:"name" gorm:"type:varchar(255)"`
+	UserID    uint   `json:"user_id"`
 }
